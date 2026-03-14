412a9da85c9f5ef2eb50aec8adb94de800d8ebe7
master9421 <maotouyee@outlook.com>
2026-03-14 15:12:34 +0800

    feat(config): 更新 Bangumi 用户ID

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -116,7 +116,7 @@ export const siteConfig: SiteConfig = {
 	// bangumi配置
 	bangumi: {
 		// Bangumi用户ID
-		userId: "1143164",
+		userId: "winteroften",
 		// 条目类型排序，数组中的类型将按顺序优先展示
 		// 可选值: "anime" | "book" | "music" | "game" | "real" (暂不支持"real"类型)
 		// 未列出的类型将按默认顺序排在后面
